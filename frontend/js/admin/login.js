9d470ecf3583f92d26564aeab06b7a07e8d3c8df
Edwar Velásquez <ricoprogramar@gmail.com>
2026-04-20 21:30:01 -0500

    fix: se corrigieron rutas del login.js

--- frontend/js/admin/login.js
+++ frontend/js/admin/login.js
@@ -1,45 +1,4 @@
-
-// import { API_BASE } from "../config.js";
-
-
-// const btnLogin = document.getElementById("btnLogin");
-// const btnCancel = document.getElementById("btnCancel");
-// const error = document.getElementById("error");
-
-// btnCancel.addEventListener("click", () => {
-//   window.location.href = "/frontend/";
-// });
-
-// btnLogin.addEventListener("click", async () => {
-//   const email = document.getElementById("email").value.trim();
-//   const password = document.getElementById("password").value.trim();
-
-//   if (!email || !password) {
-//     error.textContent = "Completa todos los campos";
-//     return;
-//   }
-
-//   const res = await fetch(`${API_BASE}/api/admin/login`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ email, password }),
-//   });
-
-//   if (!res.ok) {
-//     error.textContent = "Credenciales incorrectas";
-//     return;
-//   }
-
-//   sessionStorage.setItem("admin_auth", "true");
-//   window.location.href = "/frontend/admin/invitation.html";
-// });
-
-
-
-
-
 // frontend/js/admin/login.js
-// FIX: permitir login con tecla Enter
 
 import { API_BASE } from "../config.js";
 
@@ -50,7 +9,7 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 
 btnCancel.addEventListener("click", () => {
-  window.location.href = "/frontend/";
+  window.location.href = "/";
 });
 
 async function handleLogin() {
@@ -74,7 +33,7 @@ async function handleLogin() {
   }
 
   sessionStorage.setItem("admin_auth", "true");
-  window.location.href = "/frontend/admin/index.html";
+  window.location.href = "/admin/index.html";
 }
 
 // Click normal
